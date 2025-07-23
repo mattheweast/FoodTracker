420970615792b476f1d8dc56f64ee5c708f333d8
mattheweast <account.matteast@icloud.com>
2025-07-23 21:53:54 +0100

    Updated Files

---
+++ FoodTracker/FoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTrackerApp.swift
+//  FoodTracker
+//
+//  Created by Matt East on 23/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct FoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
